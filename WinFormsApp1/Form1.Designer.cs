fcc7752ad35e01ed4d15d10ce8f895dfd195d18e
Димитър Гогов <stu2101261032@ui-plovdiv.bg>
2025-01-10 10:33:12 +0200

    1.0

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -13,8 +13,6 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
         private void InitializeComponent()
         {
             this.txtFilePath = new System.Windows.Forms.TextBox();
@@ -22,82 +20,120 @@
             this.txtFactor = new System.Windows.Forms.TextBox();
             this.lblFactor = new System.Windows.Forms.Label();
             this.lblCreator = new System.Windows.Forms.Label();
-            this.btnDownscale = new System.Windows.Forms.Button();
-            this.btnCancel = new System.Windows.Forms.Button();
-            this.progressBar = new System.Windows.Forms.ProgressBar();
+
+            this.lblTitleLnr = new System.Windows.Forms.Label();
+            this.btnDownscaleLnr = new System.Windows.Forms.Button();
+            this.btnCancelLnr = new System.Windows.Forms.Button();
+            this.progressBarLnr = new System.Windows.Forms.ProgressBar();
+
+            this.lblTitlePrl = new System.Windows.Forms.Label();
+            this.btnDownscalePrl = new System.Windows.Forms.Button();
+            this.btnCancelPrl = new System.Windows.Forms.Button();
+            this.progressBarPrl = new System.Windows.Forms.ProgressBar();
+
             this.SuspendLayout();
-            
-            // File path field
+
+            // File Path TextBox
             this.txtFilePath.Location = new System.Drawing.Point(12, 12);
             this.txtFilePath.Size = new System.Drawing.Size(245, 23);
-            
-            // Open file button
+
+            // Open File Button
             this.btnOpenFile.Text = "Open file";
             this.btnOpenFile.AutoSize = true;
             this.btnOpenFile.Location = new System.Drawing.Point(264, 10);
             this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
-            
-            // Label downscale
+
+            // Downscale Percentage Label
             this.lblFactor.Text = "Downscale %:";
             this.lblFactor.Location = new System.Drawing.Point(12, 51);
             this.lblFactor.AutoSize = true;
 
-            // Factor field
+            // Factor TextBox
             this.txtFactor.Location = new System.Drawing.Point(18 + lblFactor.Width, 47);
-            this.txtFactor.AutoSize = true;
-            this.txtFactor.TabIndex = 1;
-            
-            // Downscale button
-            this.btnDownscale.Text = "Start downscaling";
-            this.btnDownscale.AutoSize = true;
-            this.btnDownscale.Width = 230;
-            this.btnDownscale.Location = new System.Drawing.Point(11, 80);
-            this.btnDownscale.Click += new System.EventHandler(this.btnDownscale_Click);
-            
-            // Cancel button
-            this.btnCancel.Text = "Cancel";
-            this.btnCancel.AutoSize = true;
-            this.btnCancel.Width = 100;
-            this.btnCancel.Location = new System.Drawing.Point(343 - btnCancel.Width, 80);
-            this.btnCancel.Enabled = false;
-            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
-            
-            // Progress bar
-            this.progressBar.Location = new System.Drawing.Point(12, 115);
-            this.progressBar.Size = new System.Drawing.Size(330, 23);
-            this.progressBar.TabIndex = 2;
-            
-            // Label creator
+            this.txtFactor.Size = new System.Drawing.Size(100, 23);
+
+            // Linear Downscale Section
+            this.lblTitleLnr.Text = "Linear Downscale:";
+            this.lblTitleLnr.Location = new System.Drawing.Point(12, 80);
+            this.lblTitleLnr.AutoSize = true;
+
+            this.btnDownscaleLnr.Text = "Start Linear Downscaling";
+            this.btnDownscaleLnr.AutoSize = true;
+            this.btnDownscaleLnr.Width = 230;
+            this.btnDownscaleLnr.Location = new System.Drawing.Point(12, 110);
+            this.btnDownscaleLnr.Click += new System.EventHandler(this.btnDownscaleLnr_Click);
+
+            this.btnCancelLnr.Text = "Cancel";
+            this.btnCancelLnr.AutoSize = true;
+            this.btnCancelLnr.Width = 100;
+            this.btnCancelLnr.Location = new System.Drawing.Point(343 - this.btnCancelLnr.Width, 110);
+
+            this.progressBarLnr.Location = new System.Drawing.Point(12, 145);
+            this.progressBarLnr.Size = new System.Drawing.Size(330, 23);
+
+            // Parallel Downscale Section
+            this.lblTitlePrl.Text = "Parallel Downscale:";
+            this.lblTitlePrl.Location = new System.Drawing.Point(12, 180);
+            this.lblTitlePrl.AutoSize = true;
+
+            this.btnDownscalePrl.Text = "Start Parallel Downscaling";
+            this.btnDownscalePrl.AutoSize = true;
+            this.btnDownscalePrl.Width = 230;
+            this.btnDownscalePrl.Location = new System.Drawing.Point(12, 210);
+            this.btnDownscalePrl.Click += new System.EventHandler(this.btnDownscalePrl_Click);
+
+            this.btnCancelPrl.Text = "Cancel";
+            this.btnCancelPrl.AutoSize = true;
+            this.btnCancelPrl.Width = 100;
+            this.btnCancelPrl.Location = new System.Drawing.Point(343 - this.btnCancelPrl.Width, 210);
+
+            this.progressBarPrl.Location = new System.Drawing.Point(12, 245);
+            this.progressBarPrl.Size = new System.Drawing.Size(330, 23);
+
+            // Creator Label
             this.lblCreator.Text = "Made by Dimitar Gogov";
-            this.lblCreator.Location = new System.Drawing.Point(12, 145);
+            this.lblCreator.Location = new System.Drawing.Point(12, 275);
             this.lblCreator.AutoSize = true;
-            
-            // Form
-            this.ClientSize = new System.Drawing.Size(354, 175);
-            this.Controls.Add(this.txtFilePath);
-            this.Controls.Add(this.btnOpenFile);
-            this.Controls.Add(this.lblFactor);
-            this.Controls.Add(this.txtFactor);
-            this.Controls.Add(this.btnDownscale);
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.progressBar);
-            this.Controls.Add(this.lblCreator);
+
+            // Form Settings
+            this.ClientSize = new System.Drawing.Size(354, 315);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Image Downscaler";
+
+            // Adding Controls
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.txtFactor);
+            this.Controls.Add(this.lblFactor);
+            this.Controls.Add(this.lblCreator);
+            this.Controls.Add(this.lblTitleLnr);
+            this.Controls.Add(this.btnDownscaleLnr);
+            this.Controls.Add(this.btnCancelLnr);
+            this.Controls.Add(this.progressBarLnr);
+            this.Controls.Add(this.lblTitlePrl);
+            this.Controls.Add(this.btnDownscalePrl);
+            this.Controls.Add(this.btnCancelPrl);
+            this.Controls.Add(this.progressBarPrl);
+
             this.ResumeLayout(false);
             this.PerformLayout();
         }
 
-        private System.Windows.Forms.Label label1;
-
-        #endregion
-
+        // Controls
         private System.Windows.Forms.TextBox txtFilePath;
         private System.Windows.Forms.Button btnOpenFile;
         private System.Windows.Forms.TextBox txtFactor;
         private System.Windows.Forms.Label lblFactor;
-        private System.Windows.Forms.Button btnDownscale;
-        private System.Windows.Forms.Button btnCancel;
-        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblCreator;
+
+        private System.Windows.Forms.Label lblTitleLnr;
+        private System.Windows.Forms.Button btnDownscaleLnr;
+        private System.Windows.Forms.Button btnCancelLnr;
+        private System.Windows.Forms.ProgressBar progressBarLnr;
+
+        private System.Windows.Forms.Label lblTitlePrl;
+        private System.Windows.Forms.Button btnDownscalePrl;
+        private System.Windows.Forms.Button btnCancelPrl;
+        private System.Windows.Forms.ProgressBar progressBarPrl;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,158 +1,295 @@
-namespace WinFormsApp1;
-
 using System;
 using System.Drawing;
 using System.IO;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
-public partial class Form1 : Form
+namespace WinFormsApp1
 {
-    private CancellationTokenSource cancellationTokenSource;
-    private Thread downscaleThread;
-    private Label lblCreator;
-
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
-    }
+        private CancellationTokenSource cancellationTokenSource;
+        private Bitmap inputImage;
+        private string outputFilePath;
+        private int rows = 8;  // Number of chunks for parallel downscaling
+        private int totalProgress = 0; // Variable to hold the overall progress
+        private object progressLock = new object(); // Lock to ensure thread safety for progress updates
 
-    // Open file
-    private void btnOpenFile_Click(object sender, EventArgs e)
-    {
-        using (OpenFileDialog openFileDialog = new OpenFileDialog())
+        public Form1()
         {
-            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+            InitializeComponent();
+        }
 
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
+        // Open file
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
-                txtFilePath.Text = openFileDialog.FileName;
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtFilePath.Text = openFileDialog.FileName;
+                }
             }
         }
-    }
 
-    // Downscale logic
-    private void btnDownscale_Click(object sender, EventArgs e)
-    {
-        if (!float.TryParse(txtFactor.Text, out float factor) || factor <= 0 || factor > 100)
+        // Linear Downscale
+        private void btnDownscaleLnr_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Please enter a valid percentage (1-100)!", "Invalid input!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            return;
+            StartDownscale(false);
         }
 
-        if (string.IsNullOrWhiteSpace(txtFilePath.Text) || !File.Exists(txtFilePath.Text))
+        // Parallel Downscale
+        private void btnDownscalePrl_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Please select a valid image file!", "Invalid input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            return;
+            StartDownscale(true);
         }
 
-        cancellationTokenSource = new CancellationTokenSource();
-
-        try
+        private void StartDownscale(bool isParallel)
         {
-            btnDownscale.Enabled = false;
-            btnCancel.Enabled = true;
-            progressBar.Value = 0;
+            if (!float.TryParse(txtFactor.Text, out float factor) || factor <= 0 || factor > 100)
+            {
+                MessageBox.Show("Please enter a valid percentage (1-100)!", "Invalid input!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
 
-            int newPercent = 100 - Int32.Parse(txtFactor.Text);
-            string outputFilePath = (
-                Path.Combine(Path.GetDirectoryName(txtFilePath.Text),
-                $"({newPercent}% downscaled) {Path.GetFileName(txtFilePath.Text)}")
-            );
+            if (string.IsNullOrWhiteSpace(txtFilePath.Text) || !File.Exists(txtFilePath.Text))
+            {
+                MessageBox.Show("Please select a valid image file!", "Invalid input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
 
-            Bitmap inputImage = new Bitmap(txtFilePath.Text);
-            float scaleFactor = factor / 100f;
+            cancellationTokenSource = new CancellationTokenSource();
+            totalProgress = 0;
+            progressBarLnr.Value = 0;
+            progressBarPrl.Value = 0;
 
-            // Start thread
-            downscaleThread = new Thread(() => {
-                
-                Bitmap downscaledImage = DownscaleImage(inputImage, scaleFactor, cancellationTokenSource.Token);
-                downscaledImage.Save(outputFilePath);
-                downscaledImage.Dispose();
+            try
+            {
+                inputImage = new Bitmap(txtFilePath.Text);
+                float scaleFactor = factor / 100f;
+                int newPercent = 100 - (int)factor;
 
-                Invoke(new Action(() => {
-                    
-                    MessageBox.Show($"The image is downscaled to:\n{outputFilePath}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                // Determine the compression type
+                string compressionType = isParallel ? "Parallel" : "Linear";
 
-                    string folderPath = Path.GetDirectoryName(outputFilePath);
-                    
-                    if (!string.IsNullOrEmpty(folderPath))
+                // Prepare the output file path with the compression type
+                outputFilePath = Path.Combine(
+                    Path.GetDirectoryName(txtFilePath.Text),
+                    $"(Downscaled by {newPercent}% using {compressionType} compression) {Path.GetFileName(txtFilePath.Text)}"
+                );
+
+                // Start compression on a background task
+                Task.Run(() =>
+                {
+                    if (isParallel)
                     {
-                        System.Diagnostics.Process.Start("explorer.exe", folderPath);
+                        DownscaleImageParallel(inputImage, scaleFactor, cancellationTokenSource.Token);
+                    }
+                    else
+                    {
+                        DownscaleImageLinear(inputImage, scaleFactor, cancellationTokenSource.Token);
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Linear Downscale method
+        private void DownscaleImageLinear(Bitmap inputImage, float scaleFactor, CancellationToken cancellationToken)
+        {
+            try
+            {
+                int newWidth = (int)(inputImage.Width * scaleFactor);
+                int newHeight = (int)(inputImage.Height * scaleFactor);
+
+                Bitmap outputImage = new Bitmap(newWidth, newHeight);
+
+                for (int y = 0; y < newHeight; y++)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    for (int x = 0; x < newWidth; x++)
+                    {
+                        float gx = x / (float)newWidth * inputImage.Width;
+                        float gy = y / (float)newHeight * inputImage.Height;
+
+                        int x1 = (int)Math.Floor(gx);
+                        int y1 = (int)Math.Floor(gy);
+                        int x2 = Math.Min(x1 + 1, inputImage.Width - 1);
+                        int y2 = Math.Min(y1 + 1, inputImage.Height - 1);
+
+                        Color c1 = inputImage.GetPixel(x1, y1);
+                        Color c2 = inputImage.GetPixel(x2, y1);
+                        Color c3 = inputImage.GetPixel(x1, y2);
+                        Color c4 = inputImage.GetPixel(x2, y2);
+
+                        float dx = gx - x1;
+                        float dy = gy - y1;
+
+                        Color interpolated = InterpolateColors_Linar(c1, c2, c3, c4, dx, dy);
+                        outputImage.SetPixel(x, y, interpolated);
                     }
 
-                    btnDownscale.Enabled = true;
-                    btnCancel.Enabled = false;
-                    cancellationTokenSource.Dispose();
-                    
+                    // Update progress
+                    UpdateProgress((int)((float)(y + 1) / newHeight * 100));
+
+                    // Update the progress bar in UI thread
+                    Invoke(new Action(() => progressBarLnr.Value = totalProgress));
+                }
+
+                // After compression, save and notify
+                outputImage.Save(outputFilePath);
+                Invoke(new Action(() =>
+                {
+                    MessageBox.Show($"Image saved at: {outputFilePath}", "Compression Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    System.Diagnostics.Process.Start("explorer.exe", Path.GetDirectoryName(outputFilePath));
                 }));
-                
-            });
 
-            // Start
-            downscaleThread.Start();
-        }
-        catch (Exception ex)
-        {
-            MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-        }
-    }
-
-    // Cancel logic
-    private void btnCancel_Click(object sender, EventArgs e)
-    {
-        cancellationTokenSource?.Cancel();
-        downscaleThread?.Abort();
-    }
-
-    // Get downscaled image
-    private Bitmap DownscaleImage(Bitmap inputImage, float scaleFactor, CancellationToken cancellationToken)
-    {
-        int newWidth = (int)(inputImage.Width * scaleFactor);
-        int newHeight = (int)(inputImage.Height * scaleFactor);
-
-        Bitmap outputImage = new Bitmap(newWidth, newHeight);
-
-        for (int y = 0; y < newHeight; y++)
-        {
-            cancellationToken.ThrowIfCancellationRequested();
-
-            for (int x = 0; x < newWidth; x++)
-            {
-                float gx = x / (float)newWidth * inputImage.Width;
-                float gy = y / (float)newHeight * inputImage.Height;
-
-                int x1 = (int)Math.Floor(gx);
-                int y1 = (int)Math.Floor(gy);
-                int x2 = Math.Min(x1 + 1, inputImage.Width - 1);
-                int y2 = Math.Min(y1 + 1, inputImage.Height - 1);
-
-                Color c1 = inputImage.GetPixel(x1, y1);
-                Color c2 = inputImage.GetPixel(x2, y1);
-                Color c3 = inputImage.GetPixel(x1, y2);
-                Color c4 = inputImage.GetPixel(x2, y2);
-
-                float dx = gx - x1;
-                float dy = gy - y1;
-
-                Color interpolated = InterpolateColors(c1, c2, c3, c4, dx, dy);
-                outputImage.SetPixel(x, y, interpolated);
+                outputImage.Dispose();
             }
-
-            // Update
-            Invoke(new Action(() => progressBar.Value = (int)((y / (float)newHeight) * 100)));
+            catch (OperationCanceledException)
+            {
+                MessageBox.Show("The downscaling operation was canceled.", "Operation Canceled", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred during downscaling: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
-        return outputImage;
-    }
+        // Parallel Downscale method
+        private void DownscaleImageParallel(Bitmap inputImage, float scaleFactor, CancellationToken cancellationToken)
+        {
+            try
+            {
+                // Calculate the new width and height based on the scale factor
+                int newWidth = (int)(inputImage.Width * scaleFactor);
+                int newHeight = (int)(inputImage.Height * scaleFactor);
 
-    // Get colors
-    private Color InterpolateColors(Color c1, Color c2, Color c3, Color c4, float dx, float dy)
-    {
-        float r = c1.R * (1 - dx) * (1 - dy) + c2.R * dx * (1 - dy) + c3.R * (1 - dx) * dy + c4.R * dx * dy;
-        float g = c1.G * (1 - dx) * (1 - dy) + c2.G * dx * (1 - dy) + c3.G * (1 - dx) * dy + c4.G * dx * dy;
-        float b = c1.B * (1 - dx) * (1 - dy) + c2.B * dx * (1 - dy) + c3.B * (1 - dx) * dy + c4.B * dx * dy;
+                // Block size (400x400)
+                int blockWidth = 400;
+                int blockHeight = 400;
 
-        return Color.FromArgb((int)r, (int)g, (int)b);
+                // Calculate the number of chunks horizontally and vertically
+                int horizontalChunks = (int)Math.Ceiling((double)inputImage.Width / blockWidth);
+                int verticalChunks = (int)Math.Ceiling((double)inputImage.Height / blockHeight);
+
+                // Create an output image with the new dimensions
+                Bitmap outputImage = new Bitmap(newWidth, newHeight);
+
+                // Prepare for progress tracking
+                int totalChunks = horizontalChunks * verticalChunks;
+                int chunksProcessed = 0;
+
+                // Create tasks for each chunk
+                Task[] tasks = new Task[totalChunks];
+                int taskIndex = 0;
+
+                for (int chunkY = 0; chunkY < verticalChunks; chunkY++)
+                {
+                    for (int chunkX = 0; chunkX < horizontalChunks; chunkX++)
+                    {
+                        int startX = chunkX * blockWidth;
+                        int startY = chunkY * blockHeight;
+                        int chunkWidthAdjusted = Math.Min(blockWidth, inputImage.Width - startX);
+                        int chunkHeightAdjusted = Math.Min(blockHeight, inputImage.Height - startY);
+
+                        tasks[taskIndex] = Task.Run(() =>
+                        {
+                            // Crop the chunk from the original image
+                            Rectangle chunkRect = new Rectangle(startX, startY, chunkWidthAdjusted, chunkHeightAdjusted);
+                            using (Bitmap chunkImage = inputImage.Clone(chunkRect, inputImage.PixelFormat))
+                            {
+                                // Resize (downscale) the chunk
+                                Bitmap downscaledChunk = new Bitmap(chunkImage, (int)(chunkImage.Width * scaleFactor), (int)(chunkImage.Height * scaleFactor));
+
+                                // Update the output image with the downscaled chunk
+                                using (Graphics g = Graphics.FromImage(outputImage))
+                                {
+                                    g.DrawImage(downscaledChunk, startX * scaleFactor, startY * scaleFactor);
+                                }
+
+                                // Track progress
+                                Interlocked.Increment(ref chunksProcessed);
+                                int progress = (int)((float)chunksProcessed / totalChunks * 100);
+
+                                // Update the progress bar
+                                Invoke(new Action(() =>
+                                {
+                                    progressBarPrl.Value = progress;
+                                }));
+                            }
+
+                            // Check for cancellation
+                            cancellationToken.ThrowIfCancellationRequested();
+                        });
+
+                        taskIndex++;
+                    }
+                }
+
+                // Wait for all tasks to finish
+                Task.WhenAll(tasks).Wait();
+
+                // After all chunks are processed, save the image
+                string outputFilePath = Path.Combine(
+                    Path.GetDirectoryName(txtFilePath.Text),
+                    $"(Downscaled {100 - Int32.Parse(txtFactor.Text)}%) {Path.GetFileName(txtFilePath.Text)}"
+                );
+                outputImage.Save(outputFilePath);
+
+                // Notify the user and open the folder in Explorer
+                Invoke(new Action(() =>
+                {
+                    MessageBox.Show($"Image saved at: {outputFilePath}", "Compression Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    System.Diagnostics.Process.Start("explorer.exe", Path.GetDirectoryName(outputFilePath));
+                }));
+
+                outputImage.Dispose();
+            }
+            catch (OperationCanceledException)
+            {
+                MessageBox.Show("The downscaling operation was canceled.", "Operation Canceled", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred during downscaling: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Get interpolated colors for downscaling
+        private Color InterpolateColors_Linar(Color c1, Color c2, Color c3, Color c4, float dx, float dy)
+        {
+            float r = c1.R * (1 - dx) * (1 - dy) + c2.R * dx * (1 - dy) + c3.R * (1 - dx) * dy + c4.R * dx * dy;
+            float g = c1.G * (1 - dx) * (1 - dy) + c2.G * dx * (1 - dy) + c3.G * (1 - dx) * dy + c4.G * dx * dy;
+            float b = c1.B * (1 - dx) * (1 - dy) + c2.B * dx * (1 - dy) + c3.B * (1 - dx) * dy + c4.B * dx * dy;
+
+            return Color.FromArgb((int)r, (int)g, (int)b);
+        }
+
+        // Update progress value
+        private void UpdateProgress(int progress)
+        {
+            lock (progressLock)
+            {
+                totalProgress = progress;
+            }
+        }
+
+        // Cancel Compression
+        private void btnCancelLnr_Click(object sender, EventArgs e)
+        {
+            cancellationTokenSource?.Cancel();
+        }
+
+        private void btnCancelPrl_Click(object sender, EventArgs e)
+        {
+            cancellationTokenSource?.Cancel();
+        }
     }
 }
