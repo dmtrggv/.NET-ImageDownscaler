9953ad476bb19ad364c91980a386299aff544452
Димитър Гогов <stu2101261032@ui-plovdiv.bg>
2025-01-10 16:01:49 +0200

    1.1

--- WinFormsApp1/obj/Debug/net9.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net9.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+797fa07499219bca932e3f74b7b9012095ba9f9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fea3b72175789671aec656f4019e9badb39796f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
